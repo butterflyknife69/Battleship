701747b088a60998f757a13cf6d15d8a0d87b37c
Ihor <kishmashine@gmail.com>
2023-01-11 01:47:20 +0200

    I created the logic in javascript, wrote the initial code

--- main.js
+++ main.js
@@ -1,20 +1,52 @@
-//ОГОЛОcИТИ три перемінні для зберігання позиції кожної клітини корабля.
-var location1
-var location2
-var location3
+//три перемінні  позиції кожної клітини корабля.
+var location1=3
+var location2=4
+var location3=5
 
-//ОГОЛОcИТИ перемінні для номеру  спроби в данницй момент часу. 
+//перемінна для вводу данних користувача. 
 
-var guess
+var guess;
 
-//ОГОЛОcИТИ перемінну для кількості влучень
+//перемінна для кількості влучень
 
 var hits = 0;
 
-//ОГОЛОcИТИ перемінну для кількості спроб
+// перемінна для кількості спроб
 
 var guesses = 0;
 
-//ОГОЛОcИТИ перемінну для зберігання інформації про те, потоплений корабель чи ні.
+// перемінна для зберігання інформації про те, потоплений корабель чи ні.
 
 var isSunk = false;
+
+//У цьому випадку ми перевіряємо, що перемінна isSunk все ще містить false. Як тільки корабель буде потоплений, вона стане true
+while (isSunk == false) {
+    //ми запитуємо у користувача координати пострілу
+    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+//переконаємось, що значення у діапазоні від 0 до 6.
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+        } else {
+//кількість пострілів користувача
+        guesses = guesses + 1;
+        // якщо все вірно збільшити лічильник hits
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!")
+            hits = hits + 1;
+//перевіряємо, що у корабель було зроблено три попадання
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");}
+                } else{
+                    alert("MISS");
+                }
+            }
+        }    
+    //інформація про кількість пострілів та точність
+    var stats = "You took " + guesses + " guesses to sink the battleship, " +
+    "which means your shooting accuracy was " + (3/guesses);
+    alert(stats);
+
+
+
+
