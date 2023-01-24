9f206781072a3c6a9c54c69131b579575b07acc5
Ihor <kishmashine@gmail.com>
2023-01-24 02:30:34 +0200

    Added a piece of javascript code for the random position of ships in the game!

--- main.js
+++ main.js
@@ -21,9 +21,9 @@ var model = {
     numShips: 3,
     shipsSunk: 0,
     shipLength: 3,
-    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-            { locations: ["24", "34", "44"], hits: ["", "", ""] },
-            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    ships: [{ locations: ["0", "0", "0"], hits: ["", "", ""] },
+            { locations: ["0", "0", "0"], hits: ["", "", ""] },
+            { locations: ["0", "0", "0"], hits: ["", "", ""] }],
 // Метод отримує координати пострілу. 
 fire: function(guess) {
 // код перевірки влучень
@@ -54,6 +54,48 @@ isSunk: function(ship){
         }
     }
     return true;
+},
+// рандомні позиції кораблів
+generateShipLocations: function(){
+    var locations;
+    for (var i = 0; i < this.numShips; i++){
+      do{
+        locations = this.generateShip();
+      }
+      while (this.collision(locations));
+      this.ships[i].locations = locations; 
+    }
+},
+generateShip: function(){
+    var direction = Math.floor(Math.random() * 2);
+    var row, col;
+    if (direction === 1){
+        row = Math.floor(Math.random() * this.boardSize);
+        col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+    } else{
+        row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+        col = Math.floor(Math.random() * this.boardSize);
+    }
+    var newShipLocations = [];
+    for (var i = 0; i < this.shipLength; i++){
+        if (direction === 1){
+            newShipLocations.push(row + "" + (col + i));
+        } else {
+            newShipLocations.push((row + i) + "" + col);
+        }
+    }
+    return newShipLocations;
+},
+collision: function(locations){
+    for (var i = 0; i < this.numShips; i++){
+        var ship = model.ships[i];
+        for (var j = 0; j < locations.length; j++){
+            if (ship.locations.indexOf(locations[j]) >= 0){
+                return true; 
+            } 
+        }
+    }
+    return false;
 }
 // Закриваюча дужка   model         
 };
@@ -97,11 +139,21 @@ var controller ={
 function init(){
     var fireButton = document.getElementById("fireButton");
     fireButton.onclick = handleFireButton;
+    var guessInput = document.getElementById("guessInput");
+    guessInput.onkeypress = handleKeyPress;
+    model.generateShipLocations();
 }
 function handleFireButton() {
     var guessInput = document.getElementById("guessInput");
     var guess = guessInput.value;
     controller.processGuess(guess);
     guessInput.value = "";
+   }
+   function handleKeyPress(e){
+    var fireButton = document.getElementById("fireButton");
+    if (e.keyCode === 13){
+        fireButton.click();
+        return false;
+    }
    }
    window.onload = init;
